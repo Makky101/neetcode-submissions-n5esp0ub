e8635e7a5a1f510e1d62a5fbd0544594c9f9ce91
Teniola Adeyemi <teniolaa890@gmail.com>
2026-05-20 20:49:35 +0100

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        seen = defaultdict(int)
+
+        for num in nums:
+            seen[num] += 1
+        
+        for num in seen:
+            if seen[num] == 1:
+                return num
